0b36ba3745739ce95fbcfeceb9206523b1b730ef
Bobovskiy Konstantin <kbobovskiy@yandex.ru>
2021-03-27 19:25:48 +0300

    Add email secrets into configuration storage

--- TheTaleWorker/WorkerConfiguration.cs
+++ TheTaleWorker/WorkerConfiguration.cs
@@ -4,10 +4,16 @@ using System.Text;
 
 namespace TheTaleWorker
 {
+    [Serializable]
     public class WorkerConfiguration
     {
         public string Login { get; set; }
         public string Password { get; set; }
         public string ConnectionString { get; set; }
+
+        public string EmailFrom { get; set; }
+        public string EmailTo { get; set; }
+        public string EmailLogin { get; set; }
+        public string EmailPassword { get; set; }
     }
 }
